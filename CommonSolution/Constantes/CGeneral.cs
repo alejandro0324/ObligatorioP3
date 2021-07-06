e974e90a534ee58535839a243d25e7026991a27e
nahuelmaxiperez30@gmail.com <nahuelmaxiperez30@gmail.com>
2021-07-05 21:09:37 -0300

    ok

--- CommonSolution/Constantes/CGeneral.cs
+++ CommonSolution/Constantes/CGeneral.cs
@@ -8,8 +8,13 @@ namespace CommonSolution.Constantes
 {
     public class CGeneral
     {
-        public const string FUNCIONARIO = "0";
+        public const string ACTIVO = "1";
+        public const string INACTIVO = "0";
 
-        public const string CLIENTE = "1";
+        public const string PENDIENTE = "PENDIENTE";
+        public const string ASIGNADO = "ASIGNADO";
+        public const string EN_PROCESO = "EN_PROCESO";
+        public const string RESUELTO = "RESUELTO";
+        public const string DESESTIMADO = "DESESTIMADO";
     }
 }
